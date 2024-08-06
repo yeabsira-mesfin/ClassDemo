d6b85955d1b0fc43972762b4180e8f33ccc632a0
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-06 05:01:59 -0400

    Users class

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,32 +9,68 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
+class Users extends Component{
+   constructor(){
+    this.state = {
+      showUsers:true,
+      more:'Test'
+    };
+   }
+     
+  toggleUsersHandler(){
+    this.setState((currentState)=>{
+      return {showUsers:!currentState.showUsers}
+    })
+  }
+  render(){ 
+      const usersList = (
     <ul>
       {DUMMY_USERS.map((user) => (
         <User key={user.id} name={user.name} />
       ))}
     </ul>
   );
-  let color = 'yellow'
-
-  return (
+let color = 'yellow'
+    
+    return(
     <div className={classes.users}>
-      <button onClick={toggleUsersHandler} style={{color:showUsers?color:'white', 
-      background:showUsers?'red':'green',
-      borderBlockColor:showUsers?'red':'green',outline:'none'}}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+    <button onClick={toggleUsersHandler} style={{color:showUsers?color:'white', 
+    background:showUsers?'red':'green',
+    borderBlockColor:showUsers?'red':'green',outline:'none'}}>
+      {this.state.showUsers ? 'Hide' : 'Show'} Users
+    </button>
+    {this.state.showUsers && usersList}
+  </div>
+  )
+  }
+}
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   let color = 'yellow'
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler} style={{color:showUsers?color:'white', 
+//       background:showUsers?'red':'green',
+//       borderBlockColor:showUsers?'red':'green',outline:'none'}}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
 
 export default Users;
